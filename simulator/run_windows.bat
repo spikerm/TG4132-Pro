1f0ad8fb4c6bf9948a4b055039069df5316377ad
spikerm <rmarkesteijn79@gmail.com>
2026-08-07 00:43:17 +0200

    Start simulator V3 with CRT boot sequence

--- simulator/run_windows.bat
+++ simulator/run_windows.bat
@@ -2,8 +2,8 @@
 setlocal
 where py >nul 2>nul
 if %errorlevel%==0 (
-  py -3 "%~dp0tr1604_sim_v2.py"
+  py -3 "%~dp0tr1604_sim_v3.py"
 ) else (
-  python "%~dp0tr1604_sim_v2.py"
+  python "%~dp0tr1604_sim_v3.py"
 )
 if errorlevel 1 pause
