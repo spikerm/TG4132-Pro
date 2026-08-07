b58ee46c3d1942678a09024e2dbf1954b3daa3bb
spikerm <rmarkesteijn79@gmail.com>
2026-08-07 08:40:11 +0200

    Start TR1604-Pro simulator V6.2 by default

--- simulator/run_windows.bat
+++ simulator/run_windows.bat
@@ -2,8 +2,8 @@
 setlocal
 where py >nul 2>nul
 if %errorlevel%==0 (
-  py -3 "%~dp0tr1604_sim_v61.py"
+  py -3 "%~dp0tr1604_sim_v62.py"
 ) else (
-  python "%~dp0tr1604_sim_v61.py"
+  python "%~dp0tr1604_sim_v62.py"
 )
 if errorlevel 1 pause
