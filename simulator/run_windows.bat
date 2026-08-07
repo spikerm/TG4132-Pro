c78c4a2a3702647948c4a96cb1cd87dd29301f8f
spikerm <rmarkesteijn79@gmail.com>
2026-08-07 10:05:57 +0200

    Start V7.2.2 simulator by default

--- simulator/run_windows.bat
+++ simulator/run_windows.bat
@@ -2,8 +2,8 @@
 setlocal
 where py >nul 2>nul
 if %errorlevel%==0 (
-  py -3 "%~dp0tr1604_sim_v72.py"
+  py -3 "%~dp0tr1604_sim_v722.py"
 ) else (
-  python "%~dp0tr1604_sim_v72.py"
+  python "%~dp0tr1604_sim_v722.py"
 )
 if errorlevel 1 pause
