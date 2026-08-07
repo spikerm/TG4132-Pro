07f3c22ee642029cfb346edfb2aa51bf3e4799b1
spikerm <rmarkesteijn79@gmail.com>
2026-08-07 08:25:53 +0200

    Launch standalone V6 simulator

--- simulator/run_windows.bat
+++ simulator/run_windows.bat
@@ -2,8 +2,8 @@
 setlocal
 where py >nul 2>nul
 if %errorlevel%==0 (
-  py -3 "%~dp0tr1604_sim_v51.py"
+  py -3 "%~dp0tr1604_sim_v6.py"
 ) else (
-  python "%~dp0tr1604_sim_v51.py"
+  python "%~dp0tr1604_sim_v6.py"
 )
 if errorlevel 1 pause
